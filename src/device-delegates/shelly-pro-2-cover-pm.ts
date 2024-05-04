c6e9b54d3bfa28798f8f9dd26c3e225cf8ff09fd
Paul Phillips <paul@thephillips.com.au>
2024-05-04 16:26:57 +1000

    Add Shelly Pro 2 Cover PM switch

---
+++ src/device-delegates/shelly-pro-2-cover-pm.ts
@@ -0,0 +1,23 @@
+import { ShellyPro2CoverPm } from 'shellies-pp';
+
+import { DeviceDelegate } from './base';
+
+/**
+ * Handles Shelly Pro 4PM devices.
+ */
+export class ShellyPro2CoverPmDelegate extends DeviceDelegate {
+  protected setup() {
+    const d = this.device as ShellyPro2CoverPm;
+    const isCover = d.profile === 'cover';
+
+    this.addCover(d.cover0, { active: isCover });
+    this.addCover(d.cover1, { active: isCover });
+
+    this.addSwitch(d.switch0, { active: !isCover });
+    this.addSwitch(d.switch1, { active: !isCover });
+    this.addSwitch(d.switch2, { active: !isCover });
+    this.addSwitch(d.switch3, { active: !isCover });
+  }
+}
+
+DeviceDelegate.registerDelegate(ShellyPro2CoverPmDelegate, ShellyPro2CoverPm);
